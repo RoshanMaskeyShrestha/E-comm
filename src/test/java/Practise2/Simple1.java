4191fbd286dbf99072d7b30192a97a7ef336c9fc
mmask <mmask@Roshan>
2023-04-04 10:37:34 +0530

    This is 34th commit

---
+++ src/test/java/Practise2/Simple1.java
@@ -0,0 +1,13 @@
+package Practise2;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Simple1 {
+	@Test
+	public void syso() {
+		Reporter.log("hi this is update",true);
+
+	}
+
+}
